5dddd03446195239767efdf8e304c9c87f9c6cbf
HywenSettler <samratkadian99@gmail.com>
2020-10-15 21:40:47 +0530

    fix map UI

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Grid, Button } from "semantic-ui-react";
+import { Grid, Button, Container } from "semantic-ui-react";
 import Map from "./Map";
 import TrafficLight from "./TrafficLight";
 
@@ -15,11 +15,17 @@ const Result = (props) => {
   };
 
   return (
-    <Grid>
+    <Grid
+      style={{
+        border: "1px solid black",
+        display: "flex",
+        justifyContent: "space-around",
+      }}
+    >
       <Grid.Row style={{ height: "10vh" }}>
         <div
           style={{
-            marginLeft: "50vw",
+            margin: "0 auto",
             paddingTop: "2vh",
           }}
         >
@@ -29,18 +35,26 @@ const Result = (props) => {
         </div>
       </Grid.Row>
       <Grid.Row style={{ height: "90vh" }}>
-        <Grid.Column mobile={16} tablet={6} computer={8}>
+        <Grid.Column mobile={16} tablet={6} computer={10}>
           <Map
             isMarkerShown
             googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
             loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: "100vh" }} />}
+            containerElement={
+              <div
+                style={{
+                  height: "90%",
+                  width: "90%",
+                  margin: "auto",
+                }}
+              />
+            }
             mapElement={<div style={{ height: `100%` }} />}
-            state = {props.location.state}
+            state={props.location.state}
           />
         </Grid.Column>
-        <Grid.Column mobile={16} tablet={10} computer={8}>
-          <TrafficLight state = {props.location.state}/>
+        <Grid.Column mobile={16} tablet={10} computer={6}>
+          <TrafficLight state={props.location.state} />
         </Grid.Column>
       </Grid.Row>
     </Grid>
--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from "react";
-import { Statistic } from "semantic-ui-react";
-import api from '../api';
+import React, { useState, useEffect } from "react";
+import { Statistic, Loader } from "semantic-ui-react";
+import api from "../api";
 import "./TrafficLight.css";
 
 let dummyData = [
@@ -9,26 +9,25 @@ let dummyData = [
   [20, 0, 34, 25],
   [34, 20, 0, 25],
   [25, 20, 34, 0],
-]
+];
 
-const TrafficLight = ({state}) => {
-
+const TrafficLight = ({ state }) => {
   const [loading, setLoading] = useState(true);
   const [green, setGreen] = useState(-1);
   const [timer, setTimer] = useState([0, 0, 0, 0]);
 
-  useEffect(()=>{
+  useEffect(() => {
     getTimers(-1)
-    .then(res=>{
-      setLoading(false);
-      let t = setTimers(res, -1, timer);
-      
-      controlTimer((green+1)%4, t);
-    })
-    .catch(err=>{
-      console.log(err);
-    })
-  },[]);
+      .then((res) => {
+        setLoading(false);
+        let t = setTimers(res, -1, timer);
+
+        controlTimer((green + 1) % 4, t);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   const controlTimer = (g, ti) => {
     setGreen(g);
@@ -40,57 +39,68 @@ const TrafficLight = ({state}) => {
       updateTimers(ti, count);
       console.log("here");
     }, 1000);
-   
 
     getTimers(g)
-    .then(res=>{
-      let t = setTimers(res, g, ti);
-      setTimeout(()=>{
-        clearInterval(interval);
-        controlTimer((g+1)%4, t);
-      },ti[g]*1000);
-    })
-    .catch(err=>{
-      console.log(err);
-    })
-  }
+      .then((res) => {
+        let t = setTimers(res, g, ti);
+        setTimeout(() => {
+          clearInterval(interval);
+          controlTimer((g + 1) % 4, t);
+        }, ti[g] * 1000);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const setTimers = (res, gr, ti) => {
-    let g = (gr+1)%4;
+    let g = (gr + 1) % 4;
     let timers = [...ti];
     timers[g] = res.data[g];
-    
+
     let index;
-    for(index=0 ; index<3 ; index++){
-      if(index === 0){
-        timers[(g+1)%4] = timers[g%4];
-      }else{
-        timers[(g+index+1)%4] = timers[(g+index)%4] + res.data[(g+index)%4];
+    for (index = 0; index < 3; index++) {
+      if (index === 0) {
+        timers[(g + 1) % 4] = timers[g % 4];
+      } else {
+        timers[(g + index + 1) % 4] =
+          timers[(g + index) % 4] + res.data[(g + index) % 4];
       }
     }
     return timers;
-  }
+  };
 
   const getTimers = (i) => {
-
-    return new Promise((res, rej)=>{
+    return new Promise((res, rej) => {
       res({
-        data : dummyData[i+1]
-      })
+        data: dummyData[i + 1],
+      });
     });
     // return api.get(`/${state._id}`);
-  }
+  };
   const updateTimers = (ti, count) => {
     console.log(ti);
     let i;
     let time = [...ti];
-    for(i=0 ; i<4 ; i++){
-      if(time[i] > 0) time[i] = ti[i] - count;
+    for (i = 0; i < 4; i++) {
+      if (time[i] > 0) time[i] = ti[i] - count;
     }
     setTimer(time);
-  }
+  };
 
-  if(loading) return <div>Loading...</div>
+  if (loading)
+    return (
+      <div>
+        {" "}
+        <Loader
+          active
+          inline="centered"
+          style={{
+            marginTop: "50vh",
+          }}
+        />
+      </div>
+    );
 
   return (
     <div
@@ -102,44 +112,43 @@ const TrafficLight = ({state}) => {
       }}
     >
       {new Array(4).fill("").map((_, index) => (
-        <div style={{marginLeft : "20px"}}>
+        <div style={{ marginLeft: "20px" }}>
           <Statistic
             style={{
               // marginLeft: "45vw",
-              position : "relative",
+              position: "relative",
               top: "15px",
             }}
             color="grey"
           >
-            <Statistic.Value>{String.fromCharCode(index+65)}</Statistic.Value>
+            <Statistic.Value>{String.fromCharCode(index + 65)}</Statistic.Value>
           </Statistic>
-            <div
-              key={index}
+          <div
+            key={index}
+            style={{
+              display: "inline",
+            }}
+          >
+            <input
+              type="radio"
+              name="traffic-light-color"
+              className={index === green ? "color3" : "color1"}
+              value={index === green ? "color3" : "color1"}
               style={{
-                display: "inline",
+                margin: "4vh 0vw",
               }}
+            />
+            <Statistic
+              style={{
+                position: "relative",
+                top: "15px",
+              }}
+              color={index === green ? "green" : "red"}
             >
-              <input
-                type="radio"
-                name="traffic-light-color"
-                className={index===green ? "color3" : "color1"}
-                value={index===green ? "color3" : "color1"}
-                style={{
-                  margin: "4vh 0vw"
-                }}
-              />
-              <Statistic
-                style={{
-                  position : "relative",
-                  top: "15px",
-                }}
-                color = {index===green ? 'green': 'red'}
-              >
-                <Statistic.Value>{timer[index]}</Statistic.Value>
-              </Statistic>
+              <Statistic.Value>{timer[index]}</Statistic.Value>
+            </Statistic>
           </div>
         </div>
-        
       ))}
     </div>
   );
